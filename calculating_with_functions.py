56905b19dc836755004d7194f90ae408941e2e13
raskovnik <4lch3my.3ck0@gmail.com>
2021-07-19 15:28:05 +0300

    calulating with functions

--- calculating_with_functions.py
+++ calculating_with_functions.py
@@ -1,69 +1,56 @@
-# def one(val=None):
-#     if val == None:
-#         return "1"
-#     else:
-#         return int(eval("1" + str(val)))
+def one(val=None):
+    if val == None:
+        return "1"
+    else:
+        return int(eval("1" + str(val)))
 
-# def two(val=None):
-#     if val == None:
-#         return "2"
-#     else:
-#         return int(eval("2" + str(val)))
+def two(val=None):
+    if val == None:
+        return "2"
+    else:
+        return int(eval("2" + str(val)))
 
-# def three(val=None):
-#     if val == None:
-#         return "3"
-#     else:
-#         return int(eval("3" + str(val)))
+def three(val=None):
+    if val == None:
+        return "3"
+    else:
+        return int(eval("3" + str(val)))
 
-# def four(val=None):
-#     if val == None:
-#         return "4"
-#     else:
-#         return int(eval("4" + str(val)))
+def four(val=None):
+    if val == None:
+        return "4"
+    else:
+        return int(eval("4" + str(val)))
 
-# def five(val=None):
-#     if val == None:
-#         return "5"
-#     else:
-#         return int(eval("5" + str(val)))
+def five(val=None):
+    if val == None:
+        return "5"
+    else:
+        return int(eval("5" + str(val)))
 
-# def six(val=None):
-#     if val == None:
-#         return "6"
-#     else:
-#         return int(eval("6" + str(val)))
+def six(val=None):
+    if val == None:
+        return "6"
+    else:
+        return int(eval("6" + str(val)))
 
-# def seven(val=None):
-#     if val == None:
-#         return "7"
-#     else:
-#         return int(eval("7" + str(val)))
+def seven(val=None):
+    if val == None:
+        return "7"
+    else:
+        return int(eval("7" + str(val)))
 
-# def eight(val=None):
-#     if val == None:
-#         return "8"
-#     else:
-#         return int(eval("8" + str(val)))
+def eight(val=None):
+    if val == None:
+        return "8"
+    else:
+        return int(eval("8" + str(val)))
 
-# def nine(val=None):
-#     if val == None:
-#         return "9"
-#     else:
-#         return int(eval("9" + str(val)))
-
-# def zero(val=None): return "0" if val else return int(eval("0" + str(val)))
-# def zero(val=None): return "0" if val else return int(eval("0" + str(val)))
-# def zero(val=None): return "0" if val else return int(eval("0" + str(val)))
-def three(val=None): return int(eval("3" + str(val))) if not val else : return "3"
-# def zero(val=None): return "0" if val else return int(eval("0" + str(val)))
-# def zero(val=None): return "0" if val else return int(eval("0" + str(val)))
-# def zero(val=None): return "0" if val else return int(eval("0" + str(val)))
-# def zero(val=None): return "0" if val else return int(eval("0" + str(val)))
-def eight(val=None): return int(eval("8" + str(val))) if not val else val(8)
-# def zero(val=None): return "0" if val else return int(eval("0" + str(val)))
-
-
+def nine(val=None):
+    if val == None:
+        return "9"
+    else:
+        return int(eval("9" + str(val)))
 
 def plus(val): return "+" + str(val)
 
