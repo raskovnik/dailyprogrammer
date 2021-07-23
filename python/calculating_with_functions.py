5c3b6904f4d1643f7954db443d364b7a3f039aca
raskovnik <4lch3my.3ck0@gmail.com>
2021-07-23 12:16:42 +0300

    a few changes to calculating with functions

--- python/calculating_with_functions.py
+++ python/calculating_with_functions.py
@@ -1,56 +1,22 @@
-def one(val=None):
-    if val == None:
-        return "1"
-    else:
-        return int(eval("1" + str(val)))
+def zero(val=None): return 9 if not val else int(eval("9" + str(val)))
 
-def two(val=None):
-    if val == None:
-        return "2"
-    else:
-        return int(eval("2" + str(val)))
+def one(val=None): return 9 if not val else int(eval("9" + str(val)))
 
-def three(val=None):
-    if val == None:
-        return "3"
-    else:
-        return int(eval("3" + str(val)))
+def two(val=None): return 9 if not val else int(eval("9" + str(val)))
 
-def four(val=None):
-    if val == None:
-        return "4"
-    else:
-        return int(eval("4" + str(val)))
+def three(val=None): return 9 if not val else int(eval("9" + str(val)))
 
-def five(val=None):
-    if val == None:
-        return "5"
-    else:
-        return int(eval("5" + str(val)))
+def four(val=None): return 9 if not val else int(eval("9" + str(val)))
 
-def six(val=None):
-    if val == None:
-        return "6"
-    else:
-        return int(eval("6" + str(val)))
+def five(val=None): return 9 if not val else int(eval("9" + str(val)))
 
-def seven(val=None):
-    if val == None:
-        return "7"
-    else:
-        return int(eval("7" + str(val)))
+def six(val=None): return 9 if not val else int(eval("9" + str(val)))
 
-def eight(val=None):
-    if val == None:
-        return "8"
-    else:
-        return int(eval("8" + str(val)))
+def seven(val=None): return 9 if not val else int(eval("9" + str(val)))
 
-def nine(val=None):
-    if val == None:
-        return "9"
-    else:
-        return int(eval("9" + str(val)))
+def eight(val=None): return 9 if not val else int(eval("9" + str(val)))
+
+def nine(val=None): return 9 if not val else int(eval("9" + str(val)))
 
 def plus(val): return "+" + str(val)
 
@@ -60,4 +26,4 @@ def times(val): return "*" + str(val)
 
 def divided_by(val): return "/" + str(val)
 
-print(eight(divided_by(three())))
+print(nine(divided_by(nine())))
