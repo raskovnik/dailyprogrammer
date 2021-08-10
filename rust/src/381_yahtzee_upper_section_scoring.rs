338b1b19f9c665c5d597c586402874d596fa4eeb
raskovnik <4lch3my.3ck0@gmail.com>
2021-08-10 17:33:01 +0300

    added 381 rust

---
+++ rust/src/381_yahtzee_upper_section_scoring.rs
@@ -0,0 +1,26 @@
+use std::collections::HashMap;
+use std::fs::File;
+use std::io::BufReader;
+use std::io::BufRead;
+
+
+fn main() {
+    let mut scores = HashMap::new();
+    let reader = BufReader::new(File::open("381.txt").expect("Cannot open file"));
+    // for line in reader.lines() {
+    //     println!("{:?}", line.unwrap().parse::<i32>().unwrap());
+    // }
+    for line in reader.lines() {
+        let num = line.unwrap();
+        let count = scores.entry(&num).or_insert(0);
+        *count += num.parse::<i32>().unwrap();
+    }
+    let mut val = 0;
+    for value in scores.values() {
+        if value > &val {
+            val = *value;
+        }
+
+    }
+    println!("{}", val);
+}
